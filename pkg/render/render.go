b313014ef08b34f6ba97f3afb2185e8a1eaa6cda
Lorenzo Torracchi <lorenzotorracchi@mail.com>
2020-11-06 14:53:22 +0100

    render

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -21,15 +21,21 @@ func NewAsciiWarBulletin() *AsciiWarBulletin {
 
 func (a *AsciiWarBulletin) Render(results model.Slice) {
 	globalBulletin, groupedBulletin := a.calculator.Calculate(results)
-	totTable := newTable([]string{"Average", "Median", "Min", "Max", "Total", "Errors", "Success Ratio", "Error Ratio"})
-	totTable.Append(globalBulletin.Strings())
-	totTable.Render()
+	table := newTable([]string{"Path", "Average", "Median", "Min", "Max", "Total", "Errors", "Success Ratio", "Error Ratio"})
+	table.Render()
 
-	groupTable := newTable([]string{"Path", "Average", "Median", "Min", "Max", "Total", "Errors", "Success Ratio", "Error Ratio"})
+	i := 1
 	for path, group := range groupedBulletin {
-		groupTable.Append(append([]string{path}, group.Strings()...))
+		row := append([]string{path}, group.Strings()...)
+		if i%2 == 1 {
+			table.Rich(row, []tablewriter.Colors{{tablewriter.FgCyanColor}})
+		} else {
+			table.Rich(row, []tablewriter.Colors{{tablewriter.FgWhiteColor}})
+		}
+		i++
 	}
-	groupTable.Render()
+	table.SetFooter(append([]string{"Total"}, globalBulletin.Strings()...))
+	table.Render()
 }
 
 func newTable(header []string) *tablewriter.Table {
